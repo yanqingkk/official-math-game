4af9c12cda716c88004bde2d3dda5609483f9e9c
yanqing <yanqing.zhu@education.nsw.gov.au>
2023-06-21 21:34:19 +1000

    user data, levels, locked etc, map css :")

---
+++ official-math-game/GameStatus.cs
@@ -0,0 +1,12 @@
+﻿namespace OfficialMathGame
+{
+    public enum GameLevel
+    {
+        None = 0,
+        Level1 = 1,
+        Level2 = 2,
+        Level3 = 3,
+        Level4 = 4,
+        Level5 = 5
+    }
+}
